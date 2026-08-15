599e5e747b710007d442965a79b3733777babc2a
rajporiya <rajporiya81@gmail.com>
2026-08-15 23:36:51 +0530

    feat(song): implement featured and trending songs functionality

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,3 +1,4 @@
+import { Song } from "../models/song.models.js";
 import { User } from "../models/user.models.js";
 
 export const authCallback = async (req, res, next)=>{
@@ -20,6 +21,74 @@ export const authCallback = async (req, res, next)=>{
     } catch (error) {
         console.log("Error in auth ", error);
         next()
+        
+    }
+}
+
+export const getFearuresSogs = async (req, res, next)=>{
+    try {
+        // 6 song fetch 
+        const songs = await Song.aggregate([
+            {
+                $sample : { size : 6}
+            },
+            {
+                $project: {
+                    _id : 1,
+                    title: 1,
+                    artist : 1,
+                    imageUrl : 1,
+                    audioUrl : 1,
+                }
+            }
+        ])
+        res.json(songs)
+
+    } catch (error) {
+        
+    }
+}
+export const getMadeForYou = async (req, res, next)=>{
+    try {
+        const songs = await Song.aggregate([
+            {
+                $sample : { size : 4}
+            },
+            {
+                $project: {
+                    _id : 1,
+                    title: 1,
+                    artist : 1,
+                    imageUrl : 1,
+                    audioUrl : 1,
+                }
+            }
+        ])
+        res.json(songs)
+
+    } catch (error) {
+        
+    }
+}
+export const getTrending = async (req, res, next)=>{
+    try {
+        const songs = await Song.aggregate([
+            {
+                $sample : { size : 4}
+            },
+            {
+                $project: {
+                    _id : 1,
+                    title: 1,
+                    artist : 1,
+                    imageUrl : 1,
+                    audioUrl : 1,
+                }
+            }
+        ])
+        res.json(songs)
+
+    } catch (error) {
         
     }
 }
--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { getAllSongs } from "../controller/song.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import { getFearuresSogs, getMadeForYou, getTrending } from "../controller/auth.controller.js";
 
 const router = Router()
 
 router.get('/' ,protectRoute,requireAdmin, getAllSongs)
+router.get('/featured' ,getFearuresSogs)
+router.get('/made-for-you', getMadeForYou)
+router.get('/trendig', getTrending)
 
 export default router
