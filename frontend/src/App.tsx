dc0ad557e562d4dc2f5c9af1f7331f7a3dd06d1f
rajporiya <rajporiya81@gmail.com>
2026-08-18 21:51:16 +0530

    feat : mainLayout page create

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,20 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from './pages/HomePage'
 import AuthCallbackPage from './pages/auth-callback/AuthCallbackPage'
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react"
+import MainLayout from "./layout/MainLayout"
+import ChatPage from "./pages/ChatPage"
 
 const App = () => {
   return (
     <>
        <Routes>
-        <Route path='/' element={<HomePage />} />
         <Route path='/sso-callback' element={<AuthenticateWithRedirectCallback signInForceRedirectUrl="/auth-callback" />} />
         <Route path='/auth-callback' element={<AuthCallbackPage />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/chat" element={<ChatPage />}/>
+        </Route>
        </Routes>
       </>
   )
---
+++ frontend/src/layout/MainLayout.tsx
@@ -0,0 +1,32 @@
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const MainLayout = () => {
+  const isMobile = false;
+  
+  return (
+    <div className='h-screen bg-black text-white flex flex-col'>
+      <ResizablePanelGroup orientation='horizontal' className='h-full flex flex-1 overflow-hidden p-2'>
+        <ResizablePanel defaultSize={200}  minSize={isMobile ? 0 : 10} maxSize={300}>
+          {/* left */}
+          left sidebar
+        </ResizablePanel>
+        {/* Main content */}
+
+        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+        <ResizablePanel defaultSize={isMobile ?80 : 60}>
+          <Outlet />
+        </ResizablePanel>
+
+        {/* right */}
+        <ResizablePanel defaultSize={20} minSize={0} maxSize={250} collapsedSize={0}>
+          Friend Activity 
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  )
+}
+
+export default MainLayout
