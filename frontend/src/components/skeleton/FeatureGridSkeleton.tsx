9c50ac33a8d767d476c78d48f19a1002d4e4ad8b
rajporiya <rajporiya81@gmail.com>
2026-09-05 18:51:48 +0530

    feat : Audio play pause

--- frontend/src/components/skeleton/FeatureGridSkeleton.tsx
+++ frontend/src/components/skeleton/FeatureGridSkeleton.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const FeatureGridSkeleton = () => {
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8'>
--- frontend/src/layout/AudioPlayer.tsx
+++ frontend/src/layout/AudioPlayer.tsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import { usePlayStore } from '@/stores/usePlayerStore'
+import { useEffect, useRef } from 'react'
 
 const AudioPlayer = () => {
+  const audioRef = useRef<HTMLAudioElement | null>(null)
+  const prevSongRef = useRef<string | null>(null)
+  const { currentSong, isPlaying, playNext } = usePlayStore()
+
+  useEffect(() => {
+    const audio = audioRef.current
+    if (!audio) return
+
+    if (!currentSong) {
+      audio.pause()
+      audio.removeAttribute('src')
+      prevSongRef.current = null
+      return
+    }
+
+    const songChanged = prevSongRef.current !== currentSong._id
+
+    if (songChanged) {
+      audio.src = currentSong.audioUrl
+      audio.currentTime = 0
+      prevSongRef.current = currentSong._id
+    }
+
+    if (isPlaying) {
+      audio.play().catch((error) => {
+        console.error('Failed to play audio:', error)
+      })
+    } else {
+      audio.pause()
+    }
+  }, [currentSong, isPlaying])
+
+  // song end
+  useEffect(() => {
+    const audio = audioRef.current
+
+    const handleEnded = () => {
+      playNext()
+    }
+
+    audio?.addEventListener('ended', handleEnded)
+    return () => audio?.removeEventListener('ended', handleEnded)
+  }, [playNext])
+
   return (
-    <div>AudioPlayer</div>
+    <audio ref={audioRef} preload="auto" />
   )
 }
 
