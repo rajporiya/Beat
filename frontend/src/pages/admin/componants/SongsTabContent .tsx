e65860af452649ad90ec23c0d2f79e0dde9dc49e
rajporiya <rajporiya81@gmail.com>
2026-09-09 23:23:54 +0530

    feat : admin song table create

--- frontend/src/pages/admin/componants/SongsTabContent .tsx
+++ frontend/src/pages/admin/componants/SongsTabContent .tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Music } from "lucide-react";
+import React from "react";
+import SongsTable from "./SongsTable";
 
-const SongsTabContent  = () => {
+const SongsTabContent = () => {
   return (
-    <div>SongsTabContent </div>
-  )
-}
+    <Card>
+      <CardHeader>
+        <div className="flex items-center justify-between">
+          <div>
+            <CardTitle className="flex items-center gap-2">
+              <Music className="size-5 text-emerald-500" />
+              Song Library
+            </CardTitle>
+            <CardDescription>Manage Your Trackes</CardDescription>
+          </div>
+          <Button className="bg-green-400">+ Add Songs</Button>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <SongsTable />
+      </CardContent>
+    </Card>
+  );
+};
 
-export default SongsTabContent 
+export default SongsTabContent;
---
+++ frontend/src/pages/admin/componants/SongsTable.tsx
@@ -0,0 +1,54 @@
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { useMusicStore } from "@/stores/useMusicStore";
+
+const SongsTable = () => {
+  const { songs, isLoading, err } = useMusicStore();
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center py-8">
+        <div className="text-zinc-400">Loading songs...</div>
+      </div>
+    );
+  }
+
+  if (err) {
+    return (
+      <div className="flex items-center justify-center py-8">
+        <div className="text-red-400">{err}</div>
+      </div>
+    );
+  }
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow className="hover:bg-zinc-800/50">
+          <TableHead className="w-[50px]"> </TableHead>
+          <TableHead>Title </TableHead>
+          <TableHead>Artist </TableHead>
+          <TableHead>Release Date </TableHead>
+          <TableHead className="text-right">Actions </TableHead>
+        </TableRow>
+      </TableHeader>
+
+      <TableBody>
+        {songs.map((song) => (
+          <TableRow className="hover:bg-zinc-800/50" key={song._id}>
+            <TableCell>
+              <img src={song.imageUrl} alt={song.title} className="size-10 rounded object-cover" />
+            </TableCell>
+            <TableCell className="font-medium">{song.title}</TableCell>
+            <TableCell className="font-medium">{song.artist || "Unknown Artist"}</TableCell>``
+            <TableCell className="font-medium">{song.createdAt.split("t")[0]}</TableCell>
+            <TableCell className="text-right">
+              <button className="text-zinc-300 hover:text-white">Edit</button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default SongsTable;
